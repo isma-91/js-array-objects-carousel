3902880946abe6a9769a14f9c49eb2919187c3b1
isma-91 <isma91@hotmail.it>
2022-11-08 16:05:07 +0100

    Testing a few methods to import images with JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const eleContainer = document.querySelector(".container");
+const eleSlider = document.querySelector(".slider");
+const eleThumbs = document.querySelector(".thumbs");
+const eleArrDn = document.querySelector(".arrow-down");
+const eleArrUp = document.querySelector(".arrow-up");
 const images = [
   {
     image: "01.webp",
@@ -25,3 +30,33 @@ const images = [
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
+
+// function generateImgs(obj) {
+//   return `
+// 				<img src="img/${obj.image}" alt="${obj.title}" class="slider-img">
+// 				<div class="info" >
+//         <h2>${obj.title}</h2>
+// 				<p>${obj.text}</p>
+//         </div>
+// 		`;
+// }
+
+// function generateThumbs(obj) {
+//   return `
+// 				<img src="img/${obj.image}" alt="${obj.title}" class="thumb-img">
+// 		`;
+// }
+// images.forEach((objImg) => (eleSlider.innerHTML += generateImgs(objImg)));
+
+for (let i = 0; i < images.length; i++) {
+  // eleSlider.innerHTML += generateImgs(images[i]);
+  const eleImg = document.createElement("img");
+  eleImg.src = `img/${images.image}`;
+  eleImg.classList.add("slider-img");
+  if (i === 0) {
+    eleImg.classList.add("active");
+  }
+  eleSlider.append(eleImg);
+
+  // creare i tag immagine che vanno nella sezione .thumbs
+}
